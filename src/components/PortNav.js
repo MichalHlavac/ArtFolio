758384a9ccecdde01fe2478a9f084ebdee1d9f88
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-16 20:57:09 +0200

    Update PortNav.js

--- src/components/PortNav.js
+++ src/components/PortNav.js
@@ -35,7 +35,7 @@ const Overlay= styled.span`
     max-width:400px;
     max-height:400px;
     opacity:0;
-    padding:160px 10px;
+    padding:auto 10px;
     box-sizing:border-box;
     @media (hover: none)
         { 
