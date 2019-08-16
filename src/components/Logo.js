1a0e7ca12dbe0a2bea08f4f29ce91a32d5fb9336
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-16 20:24:57 +0200

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Link} from '@reach/router'
+import {LangContext} from '../LangContext';
 const Logo=styled.img `
     margin:0px;
     padding:0px;
     width:auto;
     height:40px;
 `
-
-export default ()=>(
-    <Logo className="Logo" src="/icons/logo2.svg"/>
-);
+export default function (){
+    const [lang,setLang] = useContext(LangContext);
+    switch(lang){
+      case 'en':
+        return(
+          
+            <Link to='/en'>
+                <Logo className="Logo" src="/icons/logo2.svg"/>
+            </Link>
+            );
+      case 'cz' :
+        return(
+          
+            <Link to='/'>
+                <Logo className="Logo" src="/icons/logo2.svg"/>
+            </Link>
+            
+          );
+      default:return null;}};
