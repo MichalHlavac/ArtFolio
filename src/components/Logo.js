4dc6836e5cc7733fd800856f5bafc726a2aad4e3
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-16 20:27:59 +0200

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import styled from 'styled-components';
 import {Link} from '@reach/router'
 import {LangContext} from '../LangContext';
