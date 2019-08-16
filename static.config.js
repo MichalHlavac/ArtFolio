edf6d3a18bfb5ac2f9d755ae2a6d414e91790f9d
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-16 22:53:43 +0200

    Update static.config.js

--- static.config.js
+++ static.config.js
@@ -53,7 +53,7 @@ export default {
             <meta name="description" content="Fourth Drive is the moniker of the Israeli music producer Gal" />
             <meta name="author" content="Michal Hlaváč" />
             <meta name="keywords" content="Martina Fischmeister,ilustration" />
-            <link rel="icon" type="image/png" href="logo.png" />
+            <link rel="icon" type="image/png" href="icons/logo.png" />
           </Head>
           <Body>{children}</Body>
         </Html>
