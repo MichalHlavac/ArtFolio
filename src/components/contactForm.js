1b6068bc8c2ce213d228b85da315842a63de0806
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-17 15:20:13 +0200

    Update contactForm.js

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,25 +1,57 @@
 import React,{useState} from 'react';
 import axios from 'axios';
 
-export default ()=>{
-    const [email,setEmail]=useState('');
-    const [message,setMessage]=useState('');
-    const Submit = event =>{
-        event.preventDefault();
-        const Post ={"email":email,"message":message}
-        axios.post(
-            '/',Post , 
-            {headers: { 'Content-Type':  'application/json' },
-              })
-            .then(res => {
-              console.log(Post);
-            })};
-    
-    return(
-        <form className="CForm"  name="contact" netlify="true" onSubmit={Submit}>
-            <input type="text" value={email} onChange={e=> setEmail(e.target.value)} placeholder="email"/>
-            <textarea value={message} onChange={e=>setMessage(e.target.value)} placeholder="zprava"/>
-            <button type="submit">send</button>
-        </form>
-    );
-}
+const encode = data =>
+  Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
+
+
+    export default ()=> {
+        const [submitted,setSubmited]=useState(true)
+        const [email,setEmail]=useState('');
+        const [message,setMessage]=useState('');
+        render () {
+          return (
+            <div>
+              {submitted ? (
+                <p className="thanks">
+                  Thanks for submitting your information! We'll be in contact with you as soon as
+                  possible.
+                </p>
+              ) : (
+                <div
+                  onSubmit={async values => {
+                    try {
+                      await axios.post('/', encode({ 'form-name': 'contact', ...values }), {
+                        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                      })
+                      setSubmited(true)
+                    } catch (err) {
+                      window.alert(
+                        'There was a problem submitting your form! Try again or reload the page :)',
+                      )
+                      setSubmited(true)
+                    }
+                  }}
+                >
+                  {({ submitForm }) => (
+                    <form
+                      name="contact"
+                      netlify="true"
+                      netlify-honeypot="bot-field"
+                      onSubmit={submitForm}
+                    >
+                        <input type="text" value={email} onChange={e=> setEmail(e.target.value)} placeholder="email"/>
+                        <textarea value={message} onChange={e=>setMessage(e.target.value)} placeholder="zprava"/>
+                        
+                        <input name="bot-field" />
+                        <button type="submit">send</button>
+                    </form>
+                  )}
+                </div>
+              )}
+            </div>
+          )
+        }
+      }
