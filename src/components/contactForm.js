c26cd93ae50c53ade29969721c2c0ad0ab632b55
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-17 13:30:50 +0200

    Update contactForm.js

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
+
+export default ()=>{
+    const [email,setEmail]=useState('');
+    const [message,setMessage]=useState('');
+    const Submit = event =>{
+        event.preventDefault();
+        const Post =[{"email":email},{"message":message}]
+        axios.post(
+            '/',Post , 
+            {headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+              })
+            .then(res => {
+              console.log(Post);
+            })};
+    
+    return(
+        <form  name="contact" netlify="true" onSubmit={Submit}>
+            <input value={email} onChange={e=> setEmail(e.target.value)}/>
+            <input value={message} onChange={e=>setMessage(e.target.value)}/>
+            <button type="submit">send</button>
+        </form>
+    );
+}
