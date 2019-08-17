2d6624fdf7fd711fffe200887651f3a311d55fa6
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-17 08:08:01 +0200

    Update en.js

--- src/pages/en.js
+++ src/pages/en.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import {withRouteData} from 'react-static';
-import styled from 'styled-components';
-
 import PortNav from 'components/PortNav';
 
-const Portfolio=styled.div`
-
-`
-
 export default withRouteData( ({articlesEn}) =>(
-    <Portfolio lang="en">
+    <div lang="en">
         <PortNav articles= {articlesEn} lang='en/'/>
-    </Portfolio>
+    </div>
 ))
