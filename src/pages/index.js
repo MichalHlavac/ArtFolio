ba8172fb82212f58996a5199f3ddf9831719a0c0
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-17 08:11:17 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import {withRouteData} from 'react-static';
-import styled from 'styled-components';
-
 import PortNav from 'components/PortNav';
-
-const Portfolio=styled.div`
-position:relative;
-`
-
 
 export default withRouteData (({articles}) => (
 
-  <Portfolio>
+  <div>
     <PortNav articles= {articles} lang=''/>
-  </Portfolio>
+  </div>
 ));
