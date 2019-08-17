c8cd4de9aa84659f271d156e848ad5bc20ac7d51
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-17 19:47:00 +0200

    Update styledForm.js

--- src/components/styledForm.js
+++ src/components/styledForm.js
@@ -3,14 +3,18 @@ import styled from 'styled-components';
 const CForm=styled.div `
     grid-column:2/12;
     grid-row:3/4;
+    width:500px;
+    max-width:100%;
     .thanks{}
     .Form{
         text-align:center;
         input{display:block;
-            width:100%;};
+            width:100%;
+            padding:2px};
         textarea{display:block;
             resize: none;
             width:100%;
+            padding:2px;
             };
         button{display:block;
         margin:auto 0px};
