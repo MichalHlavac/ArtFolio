4bf3f859c88953166fce7bd1e79cab476a3f7e36
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-08-17 10:01:14 +0200

    Update styledAbout.js

--- src/components/styledAbout.js
+++ src/components/styledAbout.js
@@ -4,9 +4,9 @@ const About=styled.div `
 display:grid;
 grid-template-columns:repeat(12,8.333vw);
 grid-auto-rows:minmax(1vw,auto);
-h1{font-size:32px;}
-h2{font-size:25px;}
-p{font-size:16px}
+.h1{font-size:32px;}
+.h2{font-size:25px;}
+.p{font-size:16px}
 .portrait{
     grid-column:2/12;
     grid-row:1/2;
