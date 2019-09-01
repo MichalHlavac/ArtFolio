f84d5b38f850fe82947f480ac587449362ec22c7
MichalHlavac <hlavac.michalhlavac@gmail.com>
2019-09-01 12:06:31 +0200

    burger nav animation

--- src/components/BurgerNav.js
+++ src/components/BurgerNav.js
@@ -1,12 +1,16 @@
 import React,{useContext} from 'react';
-import styled from 'styled-components';
+import styled,{keyframes} from 'styled-components';
+import {slideInDown} from 'react-animations';
 import LangNav from '../components/LangNav'
 import Navigate from '../components/Navigate';
 import Social from '../components/SocialNW';
 import Logo from '../components/Logo';
 import {NavContext} from '../NavContext';
 
-const Nav = styled.nav`
+const fadeInAnimation = keyframes `${slideInDown}`;
+
+const OpenNav = styled.nav`
+    animation: 2s ${fadeInAnimation};
     @media only screen and (min-height:450px)  and (orientation:landscape){
         display:none;}
     @media only screen and (min-width:800px) and (orientation: portrait){
@@ -27,10 +31,7 @@ const Nav = styled.nav`
     grid-row:1/2;
     padding:10px;
   }
-  .Logo{
-    grid-column:1/6;
-    grid-row:1/2;
-    margin-left:10px;}
+ 
   .Social{
     grid-column:1/13;
     grid-row:3/4;
@@ -39,6 +40,27 @@ const Nav = styled.nav`
     grid-column:1/13;
     grid-row:2/3;
   }   
+`
+const ClosedNav=styled.nav `
+ @media only screen and (min-height:450px)  and (orientation:landscape){
+        display:none;}
+    @media only screen and (min-width:800px) and (orientation: portrait){
+        display:none;}
+    position:fixed;
+    top:0px;
+    left:0px;
+    z-index:+2;
+    background-color: #ffff;
+    width:100%;
+    padding-bottom:10px;
+    padding-top: 10px; 
+    display:grid;
+    grid-template-columns:repeat(12,8.333vw);
+    grid-auto-rows:minmax(1vw,auto);
+ .Logo{
+    grid-column:1/6;
+    grid-row:1/2;
+    margin-left:10px;}
 `
 const Img=styled.img `
     grid-column:11/13;
@@ -54,17 +76,17 @@ const Img=styled.img `
     const open=() =>setClick(true);
         if (click){
             return (
-                <Nav>
+                <OpenNav>
                     <Img src="/icons/close.svg" alt="close" onClick={close} />
                     <br/>
                     <LangNav/>
                     <Navigate/>
                     <Social/>
-                </Nav>)}
+                </OpenNav>)}
         return(
-            <Nav>
+            <ClosedNav>
                 <Logo/>
                 <Img src="/icons/menu.svg" alt="open" onClick={open}/>
-            </Nav>)};
+            </ClosedNav>)};
 
   
