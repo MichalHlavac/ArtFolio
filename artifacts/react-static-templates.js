c8935f813736fcafc4ca59b5aa88c833b8dbe1eb
Michal Hlaváč <48187991+MichalHlavac@users.noreply.github.com>
2020-08-09 19:26:45 +0200

    Updaate data

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -3,14 +3,14 @@
   
 // Template Map
 export default {
-  '/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/404.js': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/404.js').default,
-'/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/en.js': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/en.js').default,
-'/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/enInfo.js': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/enInfo.js').default,
-'/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/index.js': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/index.js').default,
-'/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/info.js': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/info.js').default,
-'/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/containers/Article': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/containers/Article').default,
-'/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/enInfo': require('/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/enInfo').default
+  '/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/404.js': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/404.js').default,
+'/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/en.js': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/en.js').default,
+'/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/enInfo.js': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/enInfo.js').default,
+'/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/index.js': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/index.js').default,
+'/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/info.js': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/info.js').default,
+'/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/containers/Article': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/containers/Article').default,
+'/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/enInfo': require('/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/enInfo').default
 }
 
-export const notFoundTemplate = '/mnt/c/Users/lahva/OneDrive/Plocha/martina web/src/pages/404.js'
+export const notFoundTemplate = '/mnt/c/Users/lahva/OneDrive/Dokumenty/GitHub/ArtFolio/src/pages/404.js'
 
