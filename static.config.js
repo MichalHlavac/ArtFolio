961ccc7f301c86e6c787877d7d196ac0cbcab5b8
Michal Hlaváč <48187991+MichalHlavac@users.noreply.github.com>
2020-08-09 19:58:43 +0200

    fix detail url path

--- static.config.js
+++ static.config.js
@@ -1,79 +1,98 @@
-import React from  'react';
-import path from 'path';
-import articles from './src/data/articles';
+import React from "react";
+import path from "path";
+import articles from "./src/data/articles";
 
 export default {
-  getRoutes: async () =>  
-     [
-      {
-        path: '/',
+  getRoutes: async () => [
+    {
+      path: "/",
+      getData: () => ({
+        articles,
+      }),
+      children: articles.map((article) => ({
+        path: `/portfolio/${article.name.replace(".", "_").toLowerCase()}`,
+        template: "src/containers/Article",
+        getData: () => ({ article, articles }),
+      })),
+    },
+    {
+      path: "/en",
+      getData: () => ({
+        articles,
+      }),
+      children: articles.map((article) => ({
+        path: `/portfolio/${article.nameEn.toLowerCase()}`,
+        template: "src/containers/Article",
         getData: () => ({
+          article,
           articles,
         }),
-        children: articles.map(article => ({
-          path: `/portfolio/${article.name.toLowerCase()}`,
-          template: 'src/containers/Article',
-          getData: () => ({article,articles,}),
-          
-        })),
-      },
-      {
-        path: '/en',
-        getData: () => ({
-          articles,
-        }),
-        children: articles.map(article => ({
-          path: `/portfolio/${article.nameEn.toLowerCase()}`,
-          template: 'src/containers/Article',
-          getData: () => ({
-            article,articles, 
-          }),
-          
-        })),
-        
-      },
-      {
-        path: '/en/info',
-        template: 'src/pages/enInfo'
-      },
-    ],
-    Document: ({
-      Html,
-      Head,
-      Body,
-      children,
-      state: { siteData, renderMeta },
-    }) => (
-        <Html lang="cs">
-          <Head>
-            <meta charSet="UTF-8"/>
-            <title>Martina Fischmeister</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="description" content="Mohu pro Vás ilustrovat knihy, články, weby, plakáty,
-             zpracovat svatební oznámení, návrh na tetování nebo vyhotovit portrét. Projektům se meze nekladou!" />
-            <meta name="author" content="Michal Hlaváč" />
-            <meta name="keywords" content="" />
-            <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-logo-16.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-logo-32.png" />
-            <meta property="og:title" content="Martina Fischmeister"/>
-            <meta property="og:description" content="Mohu pro Vás ilustrovat knihy, články, weby, plakáty,
-             zpracovat svatební oznámení, návrh na tetování nebo vyhotovit portrét. Projektům se meze nekladou!"/>
-            <meta property="og:image" content="https://martina-fischmeisterr.netlify.com/IMG/personal_work/personal_work.jpg"/>
-            <meta property="og:url" content="https://martina-fischmeisterr.netlify.com/"/>
-          </Head>
-          <Body>{children}</Body>
-        </Html>
-      ),
-  
+      })),
+    },
+    {
+      path: "/en/info",
+      template: "src/pages/enInfo",
+    },
+  ],
+  Document: ({
+    Html,
+    Head,
+    Body,
+    children,
+    state: { siteData, renderMeta },
+  }) => (
+    <Html lang="cs">
+      <Head>
+        <meta charSet="UTF-8" />
+        <title>Martina Fischmeister</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Mohu pro Vás ilustrovat knihy, články, weby, plakáty,
+             zpracovat svatební oznámení, návrh na tetování nebo vyhotovit portrét. Projektům se meze nekladou!"
+        />
+        <meta name="author" content="Michal Hlaváč" />
+        <meta name="keywords" content="" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="icons/favicon-logo-16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="icons/favicon-logo-32.png"
+        />
+        <meta property="og:title" content="Martina Fischmeister" />
+        <meta
+          property="og:description"
+          content="Mohu pro Vás ilustrovat knihy, články, weby, plakáty,
+             zpracovat svatební oznámení, návrh na tetování nebo vyhotovit portrét. Projektům se meze nekladou!"
+        />
+        <meta
+          property="og:image"
+          content="https://martina-fischmeisterr.netlify.com/IMG/personal_work/personal_work.jpg"
+        />
+        <meta
+          property="og:url"
+          content="https://martina-fischmeisterr.netlify.com/"
+        />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
+
   plugins: [
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      require.resolve("react-static-plugin-source-filesystem"),
       {
-        location: path.resolve('./src/pages'),
+        location: path.resolve("./src/pages"),
       },
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
-    require.resolve('react-static-plugin-styled-components'),
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap"),
+    require.resolve("react-static-plugin-styled-components"),
   ],
-}
+};
