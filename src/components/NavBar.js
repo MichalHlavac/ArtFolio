10c88c18d39747633a41c18f2ae8530ecf775956
Michal Hlaváč <48187991+MichalHlavac@users.noreply.github.com>
2020-09-23 21:11:29 +0200

    move soc nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,54 +1,52 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { Navigation } from './Navigation';
-import { SocialMediaNav } from './SocialMediaNav';
-import { LanguageOptions } from './LanguageOptions';
-import {BorderLine} from './BorderLine';
-import Logo from './Logo';
+import { Navigation } from "./Navigation";
+import { SocialMediaNav } from "./SocialMediaNav";
+import { LanguageOptions } from "./LanguageOptions";
+import { BorderLine } from "./BorderLine";
+import Logo from "./Logo";
 
 const NavBar = styled.nav`
-	margin: auto ;
-	max-width:1200px;
-	background-color: #ffff;
-	padding: 10px 10px 5px 10px;
-	display: grid;
-	grid-template-columns: repeat(12, 1fr);
-	grid-template-rows: 100%;
-	.Logo {
-		grid-column: 1/4;
-	}
-	.content {
-		grid-column: 4/13;
-		text-align:right;
-	}
+  margin: auto;
+  max-width: 1200px;
+  background-color: #ffff;
+  padding: 10px 10px 5px 10px;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: 100%;
+  .Logo {
+    grid-column: 1/4;
+  }
+  .content {
+    grid-column: 4/13;
+    text-align: right;
+  }
 `;
 
-const NavBarWrapper = styled.span `
-	@media only screen and (max-height: 450px) and (orientation: landscape) {
-		display: none;
-	}
-	@media only screen and (max-width: 800px) and (orientation: portrait) {
-		display: none;
-	}
-	position: fixed;
-	top: 0px;
-	right: 0px;
-	z-index: +2;
-	width: 100%;
-`
+const NavBarWrapper = styled.span`
+  @media only screen and (max-height: 450px) and (orientation: landscape) {
+    display: none;
+  }
+  @media only screen and (max-width: 800px) and (orientation: portrait) {
+    display: none;
+  }
+  position: fixed;
+  top: 0px;
+  right: 0px;
+  z-index: +2;
+  width: 100%;
+`;
 
 export default () => (
-	<NavBarWrapper>
-	<NavBar>
-		<Logo />
-		<span className='content'>
-			<Navigation />
-			<BorderLine/>
-			<SocialMediaNav />
-			<BorderLine/>
-			<LanguageOptions />
-		</span>
-	</NavBar>
-	</NavBarWrapper>
+  <NavBarWrapper>
+    <NavBar>
+      <Logo />
+      <span className="content">
+        <Navigation />
+        <BorderLine />
+        <LanguageOptions />
+      </span>
+    </NavBar>
+  </NavBarWrapper>
 );
--- src/components/styledAbout.js
+++ src/components/styledAbout.js
@@ -1,78 +1,89 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const About = styled.div`
-	display: grid;
-	grid-template-columns: repeat(12, 1fr);
-	grid-auto-rows: minmax(1fr, auto);
-	.portrait {
-		grid-column: span 12;
-		grid-row: span 1;
-		width: 300px;
-		border-radius: 50%;
-		max-width: 90%;
-		margin: auto;
-	}
-	.about {
-		@media only screen and (max-width: 800px) {
-			grid-column: 2/12;
-		}
-        text-align:center;
-		grid-column: 3/11;
-		grid-row: span 1;
-	}
-	.contacts {
-		@media only screen and (max-width: 800px) {
-			grid-column: 2/12;
-            grid-row: span 1;
-		}
-        width:100%;
-        margin:auto;
-        text-align:center;
-		grid-column: 3/11;
-		grid-row: span 1;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(1fr, auto);
+  .portrait {
+    grid-column: span 12;
+    grid-row: span 1;
+    width: 300px;
+    border-radius: 50%;
+    max-width: 90%;
+    margin: auto;
+  }
+  .about {
+    @media only screen and (max-width: 800px) {
+      grid-column: 2/12;
+    }
+    text-align: center;
+    grid-column: 3/11;
+    grid-row: span 1;
+  }
+  .contacts {
+    @media only screen and (max-width: 800px) {
+      grid-column: 2/12;
+      grid-row: span 1;
+    }
+    width: 100%;
+    margin: auto;
+    text-align: center;
+    grid-column: 3/11;
+    grid-row: span 1;
 
-		a {
-			font-family: 'Roboto', sans-serif;
-		}}
-		.clients {
-			@media only screen and (max-width: 800px) {
-				grid-column: 2/12;
-				grid-row: span 1;
-			}
-			width:100%;
-			margin:auto;
-			text-align:center;
-			grid-column: 3/11;
-			grid-row: span 1;
-	
-			a {
-				font-family: 'Roboto', sans-serif;
-			}
-	}
-	.awards {
-			@media only screen and (max-width: 800px) {
-				grid-column: 2/12;
-				grid-row: span 1;
-			}
-			width:100%;
-			margin:auto;
-			text-align:center;
-			grid-column: 3/11;
-			grid-row: span 1;
-	
-	}
-	.legal {
-			@media only screen and (max-width: 800px) {
-				grid-column: 2/12;
-				grid-row: span 1;
-			}
-			width:100%;
-			padding-top:10vh;
-			margin:auto;
-			text-align:center;
-			grid-column: 3/11;
-			grid-row: span 1;
-	
-	}
+    a {
+      font-family: "Roboto", sans-serif;
+    }
+  }
+  .clients {
+    @media only screen and (max-width: 800px) {
+      grid-column: 2/12;
+      grid-row: span 1;
+    }
+    width: 100%;
+    margin: auto;
+    text-align: center;
+    grid-column: 3/11;
+    grid-row: span 1;
+
+    a {
+      font-family: "Roboto", sans-serif;
+    }
+  }
+  .awards {
+    @media only screen and (max-width: 800px) {
+      grid-column: 2/12;
+      grid-row: span 1;
+    }
+    width: 100%;
+    margin: auto;
+    text-align: center;
+    grid-column: 3/11;
+    grid-row: span 1;
+  }
+  .contact {
+    @media only screen and (max-width: 800px) {
+      grid-column: 2/12;
+      grid-row: span 1;
+    }
+    width: 100%;
+    padding-top: 1vh;
+    margin: auto;
+    text-align: center;
+    grid-column: 3/11;
+    grid-row: span 1;
+  }
+  .legal {
+    @media only screen and (max-width: 800px) {
+      grid-column: 2/12;
+      grid-row: span 1;
+    }
+    width: 100%;
+    padding-top: 5vh;
+    margin: auto;
+    text-align: center;
+    grid-column: 3/11;
+    grid-row: span 1;
+  }
 `;
 export { About };
